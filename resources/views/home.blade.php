c812cd701ffe7750a9ae694722681106cfd10d0a
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-03-07 13:33:14 +0900

    [update]home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,12 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div>
-                    <div class="home-image">
-                    <p class="text-vertical text-white">
-                        健康と<br>食べる幸せを<br>お届けします
-                    </p>
+                <div class="wrap">
+                    <div class="home-image h-100">
+                        <p class="text-vertical text-white top-text pt-5">
+                            健康と<br>食べる幸せを<br>お届けします
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
