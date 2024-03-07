01b0e8387729496ebb66df28dfddf5447dfda0de
スギハラユタカ <119823531+sugiharaYutaka@users.noreply.github.com>
2024-03-07 21:28:46 +0900

    Revert "Future home"

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
-Route::get('/uploadVideo','NameController@show');
