b21d3ab7bf84e8b7059927b9473d3e650c6bf706
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-03-15 21:43:18 +0900

    [update] footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,13 @@
-<div class="container-fluid">
-    <div class="row border footer">
-        <div class="col-1">
-
+<link href="{{asset('assets/css/footer.css')}}" rel="stylesheet">
+<div class="container-fluid bg-color-secondary py-5">
+    <div class="row my-3">
+        <div class="col-12 d-flex justify-content-center txt-gray letter-space-2">
+            うちの子の料理番
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="col-12 d-flex justify-content-center txt-gray letter-space-2">
+            特定商取引法に基づく記述
         </div>
     </div>
 </div>
