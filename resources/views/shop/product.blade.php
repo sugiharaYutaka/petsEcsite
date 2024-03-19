e61d7a108580c57c7e8ea6970c24673100cd437d
Yutaka Sugihara <kd1336505@st.kobedenshi.ac.jp>
2024-03-20 00:26:57 +0900

    [update]

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -1,8 +1,8 @@
-@include('layouts.header')
-<link href="{{asset('assets/css/product.css')}}">
+@include('layouts.shopHeader')
+<link href="{{asset('assets/css/product.css')}}" rel="stylesheet">
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-6">
+    <div class="row row-margin pt-5">
+        <div class="col-sm-12 col-md-6 col-lg-6">
             
             <div class="container-fluid p-0">
                 <div class="row">
@@ -16,12 +16,24 @@
 
         </div>
 
-        <div class="col-6">
+        <div class="col-sm-12 col-md-6 col-lg-6">
             
             <div class="container-fluid p-0">
                 <div class="row">
-                    <div class="col-12 border">
-                        
+                    <div class="col-12">
+                        <span class="d-flex justify-content-center">鹿肉のソテー</span>
+                        <br>
+                        <span class="d-flex justify-content-start">在庫あり</span>
+                        <br>
+                        <span class="d-flex justify-content-start">原材料：</span>
+                        <span>鹿肉600g</span>
+                        <br>
+                        <span class="d-flex justify-content-end">¥1500</span>
+                        <br>
+                        <hr>
+                        <div class="d-flex justify-content-center">
+                            <button class="btn btn-secondary mx-5 w-100">カートに追加</button>
+                        </div>
                     </div>
                 </div>
             </div>
